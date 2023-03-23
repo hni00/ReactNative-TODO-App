edc56f7977fd720cbdfff96b4fae143ccdf93dc3
hni00 <aceopkm@naver.com>
2023-03-23 19:40:17 +0900

    Input Component Edit

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,8 +1,14 @@
 import { Image, StyleSheet, View } from 'react-native';
 import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
 import SafeInputView from '../components/SafeInputView';
+import { useState } from 'react';
 
 const SignInScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  console.log(email, password);
+
   return (
     <SafeInputView>
       <View style={styles.container}>
@@ -14,11 +20,15 @@ const SignInScreen = () => {
           placeholder="your@email.com"
           keyboardTyupe={KeyboardTypes.EMAIL}
           returnKeyType={ReturnKeyTypes.NEXT}
+          value={email}
+          onChangeText={(email) => setEmail(email.trim())} // trim()은 공백 제거 함수
         />
         <Input
           title={'비밀번호'}
           returnKeyType={ReturnKeyTypes.DONE}
           secureTextEntry
+          value={password}
+          onChangeText={(password) => setPassword(password.trim())}
         />
       </View>
     </SafeInputView>
